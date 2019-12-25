271755ca067000a2699d147d157249c339ef5b6b
MuhammadSaadat\Muhammad Saadat <muhammadsaadat42@gmail.com>
2019-12-25 21:09:46 +0500

    Added public pages

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = ()=>{
+    return(
+        <div>
+            <h1>About Us</h1>
+            <p className="my-1">This is sample contact keeper app using mongoose</p>
+        </div>
+    )
+};
+
+export default About;
