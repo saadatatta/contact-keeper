8cccc582090147248451ad95b0823afd93bd9051
MuhammadSaadat\Muhammad Saadat <muhammadsaadat42@gmail.com>
2020-01-18 22:56:04 +0500

    changed port to PORT

--- server.js
+++ server.js
@@ -20,5 +20,5 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server on port ${PORT}`));
